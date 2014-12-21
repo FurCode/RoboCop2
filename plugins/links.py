2a19bf5c086d3b87402e499f76dadcd7d1e1a53b
Foxlet <inquiries@comprepair.tk>
2014-12-21 09:57:25 -0500

    Fixed Parsing, Invalid Link response.

--- plugins/links.py
+++ plugins/links.py
@@ -7,6 +7,7 @@ from cloudbot.util import formatting
 
 from bs4 import BeautifulSoup
 import requests
+from lxml import html
 
 linkre = re.compile(r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))', re.I)
 
@@ -14,15 +15,21 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWe
 
 def linkfinder(link):
     blacklist = ["://youtube.com", "://youtu.be", "://www.youtube.com", "://reddit.com", "://www.reddit.com"];
-    url = match.group(1)
+    try:
+        url = link.group(1)
+    except AttributeError:
+        url = link
 
     if any(domain in url for domain in blacklist):
         return
     else:
         pass
 
-    data = requests.get(url, headers=headers)
-    page = data.html()
+    try:
+        data = requests.get(url, headers=headers)
+    except requests.exceptions.MissingSchema:
+        return "Invalid or malformed link."
+    page = data.text
     soup = BeautifulSoup(page)
     meta1 = soup.find_all('meta',attrs={'name':'Description'})
     meta2 = soup.find_all('meta',attrs={'name':'description'})
@@ -40,12 +47,12 @@ def linkfinder(link):
 
     if not descr:
         try:
-            return "\x02 Link: \x02 {}".format(title)
+            return "\x02 Link: \x02 '{}'".format(title)
         except AttributeError:
             return unkmsg
     else:
         try:
-            return "\x02 Link: \x02 {} - {}".format(title, description)
+            return "\x02 Link: \x02 '{}' - {}".format(title, description)
         except AttributeError:
             return unkmsg
 
@@ -53,8 +60,10 @@ def linkfinder(link):
 def linkmatcher(match, bot):
     switch = bot.config.get("modular", {}).get("autolink")
     if switch == "ON":
-        linkfinder(match)
+        response = linkfinder(match)
+        return response
 
 @hook.command
 def preview(text):
-    linkfinder(text)
+    response = linkfinder(text)
+    return response
