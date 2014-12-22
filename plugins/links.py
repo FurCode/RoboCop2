ef5e8665debda9efbde630d3f3197816896aea18
Foxlet <inquiries@comprepair.tk>
2014-12-22 08:16:10 -0500

    Links.py now uses LinkParser.

--- plugins/links.py
+++ plugins/links.py
@@ -3,15 +3,9 @@
 import re
 
 from cloudbot import hook
-from cloudbot.util import formatting
-
-from bs4 import BeautifulSoup
-import requests
-from lxml import html
+from cloudbot.util import linkparse
 
 linkre = re.compile(r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))', re.I)
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.'}
 
 def linkfinder(link):
     blacklist = ["://youtube.com", "://youtu.be", "://www.youtube.com", "://reddit.com", "://www.reddit.com", "://redd.it"];
@@ -25,36 +19,23 @@ def linkfinder(link):
     else:
         pass
 
+    unkmsg = "\x02 Link: \x02 {}".format("Image or other non-HTML file.")
+
     try:
-        data = requests.get(url, headers=headers)
-    except requests.exceptions.MissingSchema:
-        return "Invalid or malformed link."
-    page = data.text
-    soup = BeautifulSoup(page)
-    meta1 = soup.find_all('meta',attrs={'name':'Description'})
-    meta2 = soup.find_all('meta',attrs={'name':'description'})
-    descr = "";
+        parser = linkparse.meta(url, agent='mac')
+    except linkparse.FormatError:
+        return "Link is invalid, or site has timed out."
+    try:
+        title = parser.title()
+    except linkparse.FormatError:
+        return unkmsg
+    description = parser.description()
 
-    for tag in meta1:
-        descr += str(tag['content'])
-    for tag in meta2:
-        descr += str(tag['content'])
-
-    title = re.sub('\s+',' ',soup.title.string)
-    description = formatting.truncate_str(descr, 136)
-    unknown = "Image or other non-HTML file."
-    unkmsg = "\x02 Link: \x02 {}".format(unknown)
-
-    if not descr:
-        try:
+    if not description:
             return "\x02 Link: \x02 '{}'".format(title)
-        except AttributeError:
-            return unkmsg
     else:
-        try:
             return "\x02 Link: \x02 '{}' - {}".format(title, description)
-        except AttributeError:
-            return unkmsg
+
 
 @hook.regex(linkre)
 def linkmatcher(match, bot):
